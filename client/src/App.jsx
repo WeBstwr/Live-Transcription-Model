da692bf7b099f3b82e732413a2c9acdd679796f6
WeBstwr <ifedhawebster@gmail.com>
2025-01-16 15:02:18 +0300

    create the routes
    I have created and connected the routes for different pages.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,16 @@
+import "./assets/globals.css"
+import Dashboard from "./pages/Dashboard/Dashboard"
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 function App() {
 
   return (
     <>
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Dashboard />} />
+    </Routes>
+    </BrowserRouter>
     </>
   )
 }
